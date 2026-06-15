86e9e90a2a30f238e283c5041e19b5cebd796044
Aboobacker MK <aboobackervyd@gmail.com>
2026-06-15 20:49:09 +0530

    first commit

---
+++ lib/ruby/lsp/refactor.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "refactor/version"
+
+module Ruby
+  module Lsp
+    module Refactor
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/lsp/refactor/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Lsp
+    module Refactor
+      VERSION = "0.1.0"
+    end
+  end
+end
